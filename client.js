db4f416bc773b12d682c3bcece4fe60ef1ec3866
andrej <andreepyro@gmail.com>
2024-01-16 10:51:32 +0100

    containers next to each other

--- client.js
+++ client.js
@@ -5,6 +5,6 @@ displayView = function(){
     //code that is executed as the page is loaded.
     //You shall put your own custom code here.
     //window.alert() is not allowed to be used in your implementation.
-    window.alert("Hello TDDD97!");
+    // window.alert("Hello TDDD97!");
     };
     
