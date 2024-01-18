6b388565b0236a368417c8f6720e9b05b1340f79
andrej <andreepyro@gmail.com>
2024-01-18 11:15:55 +0100

    html reformat, start validation

--- client.js
+++ client.js
@@ -10,3 +10,38 @@ function showView(viewName) {
     let html = document.getElementById(viewName).innerHTML;
     document.getElementById("content").innerHTML = html;
 }
+
+function signUp() {
+    if (isSignUpFormValid) {
+        // TODO
+    }
+}
+
+function isSignUpFormValid() {
+    // check for blank fields
+    if (document.getElementById("").value == "") {
+        return false
+    }
+    if (document.getElementById("").value == 0) {
+        return false
+    }
+    if (document.getElementById("").value == 0) {
+        return false
+    }
+    if (document.getElementById("").value == 0) {
+        return false
+    }
+    if (document.getElementById("").value == 0) {
+        return false
+    }
+    if (document.getElementById("").value == 0) {
+        return false
+    }
+    if (document.getElementById("").value == 0) {
+        return false
+    }
+    if (document.getElementById("").value == 0) {
+        return false
+    }
+    return true
+}
