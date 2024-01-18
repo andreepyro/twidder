1a031559a1bc758912de5b9226816d865b78096c
andrej <andreepyro@gmail.com>
2024-01-18 11:04:46 +0100

    js loading

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 displayView = function(){
     // the code required to display a view
-    };
-    window.onload = function(){
-    //code that is executed as the page is loaded.
-    //You shall put your own custom code here.
-    //window.alert() is not allowed to be used in your implementation.
-    // window.alert("Hello TDDD97!");
-    };
-    
+};
+
+window.onload = function(){
+    showView("welcome-view")
+};
+
+function showView(viewName) {
+    let html = document.getElementById(viewName).innerHTML;
+    document.getElementById("content").innerHTML = html;
+}
