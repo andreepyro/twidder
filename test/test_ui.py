5cfad2124f75bbbd763f6f037a4df981287f7ea0
andrej <andreepyro@gmail.com>
2024-02-17 10:47:59 +0100

    selenium: quick fix

--- test/test_ui.py
+++ test/test_ui.py
@@ -23,7 +23,7 @@ def run_server():
 def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")  # TODO enable
+    chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(5.0)
     _reset_database()
