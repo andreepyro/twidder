ef44e69ffee3668875ded49dbde3a0db6dc71a18
andrej <andreepyro@gmail.com>
2024-02-17 13:49:43 +0100

    selenium: add more tests

--- test/test_ui.py
+++ test/test_ui.py
@@ -96,91 +96,72 @@ def test_sign_in(driver):
 
     # log in with valid credentials
     driver.get("http://localhost:8080/")
-    _login(driver, "peter@parker.com", "secretpassword", True)
 
 
 def test_browse_users(driver):
-    pass
+    # create first user and logout
+    driver.get("http://localhost:8080/")
+    _register(driver, "Peter", "Parker", "Male", "Linkoping", "Sweden", "peter@parker.com", "secretpassword", True)
+    time.sleep(4.5)  # for pop-up message to disappear
+    _logout(driver)
+
+    # create second user
+    driver.get("http://localhost:8080/")
+    _register(driver, "Adam", "Cool", "Other", "Copenhagen", "Denmark", "adam@cool.com", "longpassword", True)
+    time.sleep(4.5)  # for pop-up message to disappear
+
+    # switch to browse tab
+    _switch_tab(driver, "browse")
+
+    # browse user
+    _browse_user(driver, "peter@parker.com", True)
+
+    # check correct details
+    _check_browse_tab_user_info(driver, "Peter", "Parker", "Male", "Linkoping", "Sweden", "peter@parker.com")
 
 
 def test_post_message(driver):
-    pass
-
-
-def test_change_details(driver):
-    pass
-
-
-@pytest.mark.skip("legacy test, doesn't work anymore")
-def test_e2e_workflow(driver):
-    # try to log in with invalid credentials
-    _login(driver, "random@email.com", "password", False)
-
-    # register new user
-    _register(driver, "Peter", "Parker", "Male", "Linkoping", "Sweden", "peter@parker.com", "secretpassword", True)
-
-    # check user information in home tab
-    _check_home_tab_user_info(driver, "Peter", "Parker", "Male", "Linkoping", "Sweden", "peter@parker.com")
-
-    # check url is the same after page refresh
-    old_url = driver.current_url
+    # create a new user
     driver.get("http://localhost:8080/")
-    assert driver.current_url == old_url
-
-    # log out
-    _logout(driver)
-
-    # try to register a new user with the same email
-    _register(driver, "Anna", "Great", "Female", "Stockholm", "Sweden", "peter@parker.com", "anotherpassword", False)
-
-    # try to register a new user with a new email
-    _register(driver, "Anna", "Great", "Female", "Stockholm", "Sweden", "anna@great.com", "anotherpassword", True)
-
-    # check user information in home tab
-    _check_home_tab_user_info(driver, "Anna", "Great", "Female", "Stockholm", "Sweden", "anna@great.com")
+    _register(driver, "Peter", "Parker", "Male", "Linkoping", "Sweden", "peter@parker.com", "secretpassword", True)
+    time.sleep(4.5)  # for pop-up message to disappear
 
     # add several posts
-    anna_posts = ["hey!", "how are you?", "hmmm, I like food"]
-    for post in anna_posts:
-        _add_home_post(driver, "anna@great.com", post)
-
-    # TODO add post on Peter's wall
-
-    # log out
-    _logout(driver)
-
-    # try to log in with invalid credentials
-    _login(driver, "peter@parker.com", "anotherpassword", False)
-
-    # try to log in with invalid credentials
-    _login(driver, "anna@great.com", "secretpassword", False)
-
-    # try to log in with valid credentials
-    _login(driver, "peter@parker.com", "secretpassword", True)
-
-    # switch tabs several times
-    _switch_tab(driver, "home")
-    _switch_tab(driver, "browse")
-    _switch_tab(driver, "account")
-    _switch_tab(driver, "account")
-    _switch_tab(driver, "home")
-    _switch_tab(driver, "account")
-    _switch_tab(driver, "home")
-
-    # add several posts
-    peter_posts = ["hello", "this is my second post!"]
-    for post in peter_posts:
+    posts = ["hey!", "how are you?", "Hmmm, I like food"]
+    for post in posts:
         _add_home_post(driver, "peter@parker.com", post)
 
-    # browse for another user
-    _switch_tab(driver, "browse")
-    # TODO browse for Anna
+    # reload page and check if posts still exist
+    driver.get("http://localhost:8080/")
+    time.sleep(1.0)  # wait until the app is loaded
+    for post in posts:
+        _check_post_exists(driver, "peter@parker.com", post)
 
-    # TODO check Anna's posts
 
-    # TODO add post on Anna's wall
+def test_change_account_details(driver):
+    # create a new user
+    driver.get("http://localhost:8080/")
+    _register(driver, "Peter", "Parker", "Male", "Linkoping", "Sweden", "peter@parker.com", "secretpassword", True)
+    time.sleep(4.5)  # for pop-up message to disappear
 
-    # TODO add new posts to Anna's wall
+    # check current user details in home tab
+    _check_home_tab_user_info(driver, "Peter", "Parker", "Male", "Linkoping", "Sweden", "peter@parker.com")
+
+    # switch to account tab
+    _switch_tab(driver, "account")
+
+    # change user details
+    _update_user_details(driver, "Martin", "Boss", "Female", "Stockholm", "Sweden", True)
+    time.sleep(4.5)  # for pop-up message to disappear
+
+    # check new user details in home tab
+    _switch_tab(driver, "home")
+    _check_home_tab_user_info(driver, "Martin", "Boss", "Female", "Stockholm", "Sweden", "peter@parker.com")
+
+    # reload and check user details again
+    driver.get("http://localhost:8080/")
+    time.sleep(1.0)  # wait until the app is loaded
+    _check_home_tab_user_info(driver, "Martin", "Boss", "Female", "Stockholm", "Sweden", "peter@parker.com")
 
 
 def _reset_database():
@@ -314,7 +295,18 @@ def _check_home_tab_user_info(driver: webdriver.Chrome, firstname: str, lastname
     assert driver.find_element("id", "home-user-location").get_attribute('innerHTML') == f"{city}, {country}"
 
 
-def _switch_tab(driver: webdriver.Chrome, tabname: str):
+def _check_browse_tab_user_info(driver: webdriver.Chrome, firstname: str, lastname: str, gender: str, city: str, country: str, email: str):
+    # check home tab is loaded
+    assert driver.current_url == f"http://localhost:8080/browse?email={email}"
+
+    # check user information
+    assert driver.find_element("id", "browse-user-name").get_attribute('innerHTML') == f"{firstname} {lastname}"
+    assert driver.find_element("id", "browse-user-gender").get_attribute('innerHTML') == gender
+    assert driver.find_element("id", "browse-user-email").get_attribute('innerHTML') == email
+    assert driver.find_element("id", "browse-user-location").get_attribute('innerHTML') == f"{city}, {country}"
+
+
+def _switch_tab(driver: webdriver.Chrome, tab_name: str):
     # check popup message is not visible
     popup_message = driver.find_element("id", "pop-message")
     assert popup_message.value_of_css_property('visibility') == "hidden"
@@ -323,14 +315,42 @@ def _switch_tab(driver: webdriver.Chrome, tabname: str):
     assert driver.current_url != "http://localhost:8080/"
 
     # switch tab
-    driver.find_element("id", f"sidebar-tab-{tabname}").click()
+    driver.find_element("id", f"sidebar-tab-{tab_name}").click()
 
     # check popup message is not visible
     popup_message = driver.find_element("id", "pop-message")
     assert popup_message.value_of_css_property('visibility') == "hidden"
 
     # check success
-    assert driver.current_url == f"http://localhost:8080/{tabname}"
+    assert driver.current_url == f"http://localhost:8080/{tab_name}"
+
+
+def _browse_user(driver: webdriver.Chrome, email: str, expect_success: bool):
+    # check popup message is not visible
+    popup_message = driver.find_element("id", "pop-message")
+    assert popup_message.value_of_css_property('visibility') == "hidden"
+
+    # check user is in the correct tab
+    assert driver.current_url == "http://localhost:8080/browse"
+
+    # try log in with provided credentials
+    input_email = driver.find_element("id", "input-user-email")
+    input_email.clear()
+    input_email.send_keys(email)
+
+    search_button = driver.find_element("id", "browse-search-button")
+    search_button.click()
+
+    # check operation result
+    time.sleep(1)
+    assert driver.current_url == f"http://localhost:8080/browse?email={email}"
+    popup_message = driver.find_element("id", "pop-message")
+    if expect_success:
+        assert popup_message.value_of_css_property('visibility') == "hidden"
+        assert popup_message.get_attribute('innerHTML') == ""
+    else:
+        assert popup_message.value_of_css_property('visibility') == "visible"
+        assert popup_message.get_attribute('innerHTML') == "User not found."
 
 
 def _add_home_post(driver: webdriver.Chrome, email: str, content: str):
@@ -349,11 +369,71 @@ def _add_home_post(driver: webdriver.Chrome, email: str, content: str):
     submit_button = driver.find_element("id", "button-home-new-post")
     submit_button.click()
 
+    # wait for the request
+    time.sleep(1)
+
+    # check post exists and they're correct
+    _check_post_exists(driver, email, content)
+
+
+def _check_post_exists(driver: webdriver.Chrome, email: str, content: str):
+    # check popup message is not visible
+    popup_message = driver.find_element("id", "pop-message")
+    assert popup_message.value_of_css_property('visibility') == "hidden"
+
+    # check we are on home page
+    assert driver.current_url == "http://localhost:8080/home"
+
     # check post exists
-    posts = driver.find_elements("xpath", "//div[@class='home-post']")
+    posts = driver.find_elements("xpath", "//div[@class='user-post']")
     for post in posts:
         if post.find_element("class name", "author").get_attribute('innerHTML') == email and post.find_element("class name", "content").get_attribute(
-                'innerHTML') == content:
+                'innerHTML') == content.replace("\n", "<br>") + "<br>":
             break
     else:
         assert False  # post not found!
+
+
+def _update_user_details(driver: webdriver.Chrome, firstname: str, lastname: str, gender: str, city: str, country: str, expect_success: bool):
+    # check popup message is not visible
+    popup_message = driver.find_element("id", "pop-message")
+    assert popup_message.value_of_css_property('visibility') == "hidden"
+
+    # check account tab is loaded
+    assert driver.current_url == "http://localhost:8080/account"
+
+    # fill new account details
+    input_firstname = driver.find_element("id", "input-account-first-name")
+    input_firstname.clear()
+    input_firstname.send_keys(firstname)
+
+    input_lastname = driver.find_element("id", "input-account-last-name")
+    input_lastname.clear()
+    input_lastname.send_keys(lastname)
+
+    select_gender = Select(driver.find_element("id", "input-account-gender"))
+    if gender:
+        select_gender.select_by_visible_text(gender)
+
+    input_city = driver.find_element("id", "input-account-city")
+    input_city.clear()
+    input_city.send_keys(city)
+
+    input_country = driver.find_element("id", "input-account-country")
+    input_country.clear()
+    input_country.send_keys(country)
+
+    submit_button = driver.find_element("id", "button-update-account-details")
+    submit_button.click()
+
+    # check operation result
+    time.sleep(1)
+    popup_message = driver.find_element("id", "pop-message")
+    if expect_success:
+        assert driver.current_url == "http://localhost:8080/account"
+        assert popup_message.value_of_css_property('visibility') == "visible"
+        assert popup_message.get_attribute('innerHTML') == "Account details successfully changed!"
+    else:
+        assert driver.current_url == "http://localhost:8080/account"
+        assert popup_message.value_of_css_property('visibility') == "visible"
+        assert popup_message.get_attribute('innerHTML') == "error"
