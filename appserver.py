740b3bf557a329ce6fd7ebb00fd03ee44e138d09
andrej <andreepyro@gmail.com>
2024-02-19 10:17:24 +0100

    set up logger for gunicorn server

--- appserver.py
+++ appserver.py
@@ -1,5 +1,11 @@
+import logging
+
 from twidder.database_handler import initialize_database
 from twidder.server import app
+
+logger = logging.getLogger("Twidder")
+app.logger.handlers.extend(logger.handlers)
+app.logger.setLevel(logging.DEBUG)
 
 with app.app_context():
     initialize_database()
